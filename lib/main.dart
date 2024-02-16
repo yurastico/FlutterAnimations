4e075163c6412d9bda880fd79aeb4410fc52d717
yurastico <cunha.ysc@gmail.com>
2024-02-16 19:45:43 -0300

    creating fist animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(const LogoApp());
+
 class LogoApp extends StatefulWidget {
-const LogoApp({super.key});
-@override
-State<LogoApp> createState() => _LogoAppState();
+  const LogoApp({super.key});
+  @override
+  State<LogoApp> createState() => _LogoAppState();
 }
-class _LogoAppState extends State<LogoApp> {
-@override
-Widget build(BuildContext context) {
- return Center(
- child: Container(
- margin: const EdgeInsets.symmetric(vertical: 10),
- height: 300,
- width: 300,
- child: const FlutterLogo(),
- ),
- );
-}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2),
+         vsync: this);
+
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+
+      controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: const FlutterLogo(),
+      ),
+    );
+  }
 }
