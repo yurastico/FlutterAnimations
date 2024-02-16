9274018b23bbbb141ab907eb3735ddb0026de580
yurastico <cunha.ysc@gmail.com>
2024-02-16 19:29:53 -0300

    setting up project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const LogoApp());
+class LogoApp extends StatefulWidget {
+const LogoApp({super.key});
+@override
+State<LogoApp> createState() => _LogoAppState();
+}
+class _LogoAppState extends State<LogoApp> {
+@override
+Widget build(BuildContext context) {
+ return Center(
+ child: Container(
+ margin: const EdgeInsets.symmetric(vertical: 10),
+ height: 300,
+ width: 300,
+ child: const FlutterLogo(),
+ ),
+ );
+}
+}
